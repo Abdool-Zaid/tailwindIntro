d72b717e940eb037bd614c4d081a58d675a3a2ae
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-10-18 09:36:28 +0200

    'functionality'

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <script setup>
-let startTime
-let spanWidth=
-window.innerWidth
-let spanHeight=
-window.innerHeight
-let xCoor=
-Math.round(spanWidth * Math.random())
-let yCoor=
-Math.round(spanHeight * Math.random())
-function runTrial(){
-  if (startTime==null){
-    startTime= Date.now()
-    document.querySelector('#target').style
-
+let startTime;
+let spanWidth = window.innerWidth;
+let spanHeight = window.innerHeight;
+let xCoor = Math.round(spanWidth * Math.random()) + "px";
+let yCoor = Math.round(spanHeight * Math.random()) + "px";
+function generateRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color
+  }
+function moveTarget() {
+  document.querySelector("#target").style = `
+  color: ${generateRandomColor()};
+  top: ${yCoor};
+  left: ${xCoor};
+    `;
+    document.querySelector('body').style=`
+    background-color: ${generateRandomColor()};
+    `
+}
+function runTrial() {
+  if (startTime == null) {
+    startTime = Date.now();
+    moveTarget();
     
+    document.querySelector('button').style=`display: none;`
+  } else if (startTime > 0) {
+    xCoor = Math.round(spanWidth * Math.random()) + "px";
+    yCoor = Math.round(spanHeight * Math.random()) + "px";
+    moveTarget();
+    document.querySelector('h1').innerHTML=`
+    ${
+      (Date.now()-startTime)/1000
+    } seconds
+    `
+    startTime = Date.now();
   }
 }
 </script>
 
 <template>
   <h1>Reaction timer</h1>
-  <h4></h4>
   <button @click="runTrial()">begin trial</button>
   <p></p>
   <div id="target" @click="runTrial()"><p>._.</p></div>
@@ -28,4 +51,11 @@ function runTrial(){
 
 <style scoped>
 
+#target {
+  z-index: 10;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
